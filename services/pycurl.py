a29006317e00b7db31cad50efc5008f0a8706eb7
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2014-05-02 10:11:11 +0200

    Give a bit of love to pycurl holon service.

--- services/pycurl.py
+++ services/pycurl.py
@@ -1,22 +1,20 @@
+from __future__ import absolute_import
+from . import HttpService
+from . import Response
 from StringIO import StringIO
-import HttpService
 import pycurl
-import Response
-import time
+# import time
 
 
 assert pycurl.version_info()[1] >= "7.19"
 
 
 class PyCurlHttpService(HttpService):
-    """
-    HttpService using extra-fast pycurl.
-    """
+    """HttpService using extra-fast pycurl."""
 
     def __init__(self, *args, **kwargs):
         super(PyCurlHttpService, self).__init__(*args, **kwargs)
         pycurl.global_init(pycurl.GLOBAL_ALL)
-
 
     def call(self, body):
          # to collect response data
@@ -37,20 +35,26 @@ class PyCurlHttpService(HttpService):
             "Accept: application/json",
         ])
 
-        start_time = time.time()
+        # start_time = time.time()
 
         # the actual call
         try:
             curl.perform()
             code = curl.getinfo(pycurl.HTTP_CODE)
+            # start_transfer_time = curl.getinfo(pycurl.STARTTRANSFER_TIME)
+            total_time = curl.getinfo(pycurl.TOTAL_TIME)
             curl.close()
         except pycurl.error, err:
             # raise common error class
             raise self.communication_error_class(err[0], err[1])
 
-        end_time = time.time()
+        # total_time = time.time() - start_time
 
         data = data.getvalue()
         data = unicode(data, "utf-8")
 
-        return Response(code, data, (end_time-start_time)*1000)
+        return Response(code, data, total_time*1000)
+
+    @property
+    def protocol(self):
+        return self.base_url.split('://')[0].upper()
