e3dcbb6d74fa124ef9a7e47a6e374d370d423038
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-05-22 18:34:57 +0200

    Using `setup` and `find_packages` from `setuptools`.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ compatible with its jython/corba-interfaces.
 For txtr-reaktor API see http://txtr.com/reaktor/api/
 """
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name='holon',
@@ -14,7 +15,7 @@ setup(
     author='txtr web team',
     author_email='web-dev@txtr.com',
     url='https://github.com/txtr/holon/',
-    packages=['holon'],
+    packages=find_packages(exclude=['examples',]),
     platforms='any',
     install_requires=['pycurl>=7.19.3.1'],
 )
