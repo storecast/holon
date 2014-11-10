9940a5fe068d1c26a63764455dcfc85bbaf2bced
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2014-11-10 14:11:47 +0100

    SKNS-4966 | Provide test cases for data converters.

--- holon/tests.py
+++ holon/tests.py
@@ -1,15 +1,33 @@
-from mock import Mock
-from reaktor import Reaktor
+from contextlib import contextmanager
+from mock import Mock, patch
+from reaktor import Reaktor, ReaktorObject
 from services import HttpService
 import unittest
 
 
 class HttpServiceMock(HttpService):
+    """This one is useful to test the base service methods."""
     pass
+    protocol = 'HTTP/1.1'
 
 
 class ServiceMock(HttpService):
+    """This one is useful to check the service is contructed as expected."""
     __init__ = Mock(return_value=None)
+    protocol = 'HTTP/1.1'
+
+
+@contextmanager
+def patch_json(reaktor, res='null', err='null'):
+    from services import Response
+    resp = u"""{{"error":{error},"result":{result}}}"""
+    resp = resp.format(error=err, result=res)
+    call_orig = reaktor.http_service.call
+    try:
+        reaktor.http_service.call = Mock(return_value=Response(200, resp, 0))
+        yield reaktor.http_service.call
+    finally:
+        reaktor.http_service.call = call_orig
 
 
 reaktor_config = {
@@ -71,4 +89,28 @@ class ReaktorInterfaceTestCase(unittest.TestCase):
         from functools import partial
         self.reaktor.call = partial(call_mock, headers={'head': 'bang'})
         self.reaktor.RpcInterface.RpcFunction('java?')
-        call_mock.assert_called_with('RpcInterface.RpcFunction', 'java?', headers={'head': 'bang'})
+        call_mock.assert_called_with('RpcInterface.RpcFunction', ('java?', ),
+                                     headers={'head': 'bang'})
+
+
+class ReaktorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.reaktor = Reaktor(**reaktor_config)
+
+    @patch('holon.reaktor.id_generator', return_value='')
+    def test_reaktor_call_default_converter(self, _):
+        """Default reaktor data converter is `ReaktorObject.to_reaktorobject`."""
+        with patch_json(self.reaktor, '{"prop":"value"}'):
+            r = self.reaktor.call('Interface.Method', [])
+            self.assertIsInstance(r, ReaktorObject)
+            self.assertTrue(hasattr(r, 'prop'))
+            self.assertEqual(r.prop, 'value')
+
+    @patch('holon.reaktor.id_generator', return_value='')
+    def test_reaktor_call_custom_converter(self, _):
+        """Custom reaktor data converter."""
+        custom_converter = Mock()
+        with patch_json(self.reaktor, '{"prop":"value"}'):
+            r = self.reaktor.call('Interface.Method', [],
+                                  data_converter=custom_converter)
+            custom_converter.assert_called_with({u'prop': u'value'})
